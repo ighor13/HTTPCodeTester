675125d0a13b0a4b2af5a5ea9a246e27be60c27d
ighor13 <ighor@ighor.ru>
2020-01-25 10:22:48 +0300

    v0.3a

--- tester/main.cpp
+++ tester/main.cpp
@@ -4,12 +4,14 @@
 
 int main(int argc, char *argv[])
 {
+#if defined Q_OS_WIN
     QStringList paths = QCoreApplication::libraryPaths();
     paths.append(".");
     paths.append("platforms");
 //    paths.append("imageformats");
 //    paths.append("sqldrivers");
     QCoreApplication::setLibraryPaths(paths);
+#endif
 
     QApplication a(argc, argv);
     MainWindow w;
