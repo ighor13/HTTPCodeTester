ba28781b8e0a14fa87023922cb962d5449f8a170
ighor13 <ighor@ighor.ru>
2020-10-21 11:46:34 +0300

    v0.44a

--- tester/mainwindow.cpp
+++ tester/mainwindow.cpp
@@ -117,14 +117,14 @@ void MainWindow::on_actionCopy_To_Clipboard_triggered()
          if(ui->tableWidget->item(i,0))
          {
                text+=ui->tableWidget->item(i,0)->text();
-               html+="<td  style=\"color: #"+ui->tableWidget->item(i,0)->foreground().color().name()+"\">"+ui->tableWidget->item(i,0)->text()+"</td>";
+               html+="<td  style=\"color: #"+ui->tableWidget->item(i,0)->foreground().color().name()+"\">"+ui->tableWidget->item(i,0)->text().toHtmlEscaped()+"</td>";
          }
 
          for(int j=1;j<=ui->tableWidget->columnCount();j++)
                if(ui->tableWidget->item(i,j))
                {
                            text+=QString("\t")+ui->tableWidget->item(i,j)->text();
-                           html+="<td  style=\"color: "+ui->tableWidget->item(i,j)->foreground().color().name()+"\">"+ui->tableWidget->item(i,j)->text()+"</td>";
+                           html+="<td  style=\"color: "+ui->tableWidget->item(i,j)->foreground().color().name()+"\">"+ui->tableWidget->item(i,j)->text().toHtmlEscaped()+"</td>";
                }
                else
                {
